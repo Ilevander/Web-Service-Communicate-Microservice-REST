408c0e95f7479eee04f560dd9b62c022eb6eb2a2
dell <dell@DESKTOP-4P8U8AR>
2025-01-15 20:37:41 +0100

    initilizing Project

---
+++ src/main/java/com/ilyass/webservicecommunicatemicroservicerest/WebServiceCommunicateMicroserviceRestApplication.java
@@ -0,0 +1,13 @@
+package com.ilyass.webservicecommunicatemicroservicerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceCommunicateMicroserviceRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceCommunicateMicroserviceRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilyass/webservicecommunicatemicroservicerest/WebServiceCommunicateMicroserviceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyass.webservicecommunicatemicroservicerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceCommunicateMicroserviceRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
